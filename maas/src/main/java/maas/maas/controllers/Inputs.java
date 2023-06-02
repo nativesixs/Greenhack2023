dd8e400356492a93a94bfef8b168c8b4be064442
nativesixs <matejzencak@seznam.cz>
2023-06-02 17:56:48 +0200

    floorplan issues

--- maas/src/main/java/maas/maas/controllers/Inputs.java
+++ maas/src/main/java/maas/maas/controllers/Inputs.java
@@ -1,2 +1,88 @@
-package maas.maas.controllers;public class Inputs {
+package maas.maas.controllers;
+
+
+public class Inputs {
+
+    private String ckodField;
+    private String simckodField;
+    private String dpuckodField;
+    private String inventoryField;
+    private int mode;
+    private String lvmField;
+    private String platoField;
+    private String placeckodField;
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlaceckodField() {
+        return placeckodField;
+    }
+
+    public void setPlaceckodField(String placeckodField) {
+        this.placeckodField = placeckodField;
+    }
+
+    public String getLvmField() {
+        return lvmField;
+    }
+
+    public void setLvmField(String lvmField) {
+        this.lvmField = lvmField;
+    }
+
+    public String getPlatoField() {
+        return platoField;
+    }
+
+    public void setPlatoField(String platoField) {
+        this.platoField = platoField;
+    }
+
+    public String getSimckodField() {
+        return simckodField;
+    }
+
+    public void setSimckodField(String simckodField) {
+        this.simckodField = simckodField;
+    }
+
+    public String getDpuckodField() {
+        return dpuckodField;
+    }
+
+    public void setDpuckodField(String dpuckodField) {
+        this.dpuckodField = dpuckodField;
+    }
+
+    public String getCkodField() {
+        return ckodField;
+    }
+
+    public void setCkodField(String ckodField) {
+        this.ckodField = ckodField;
+    }
+
+    public String getInventoryField() {
+        return inventoryField;
+    }
+
+    public void setInventoryField(String inventoryField) {
+        this.inventoryField = inventoryField;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
 }
--- maas/src/main/java/maas/maas/controllers/PagesController.java
+++ maas/src/main/java/maas/maas/controllers/PagesController.java
@@ -1,2 +1,68 @@
-package maas.maas.controllers;public class PagesController {
+package maas.maas.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@Controller
+public class PagesController {
+//    @Value("${graphql.url:none}")
+//    private String url;
+
+
+    @GetMapping("/firstfloor")
+    public String maasGet(Model model) {
+        model.addAttribute("data", new maas.maas.controllers.Inputs());
+//        model.addAttribute("logger","");
+        return "firstfloor";
+    }
+
+    @RequestMapping("/firstfloor/*")
+    public String index(@RequestParam(value = "action", required = false) String action, Model model,Inputs form) {
+        model.addAttribute("data", form);
+        model.addAttribute("action", action);
+        System.out.println(action);
+
+//        model.addAttribute("userIds", userIds);
+        return "firstfloor";
+    }
+
+
+
+
+//    @PostMapping("/firstfloor")
+//    public String maasSub(@ModelAttribute maas.maas.controllers.Inputs form, Model model) throws IOException, ExecutionException, InterruptedException{
+//        model.addAttribute("data", form);
+//
+////        String log = maas.maas.main(ckod,mode, inventory,url);
+////        model.addAttribute("logger",log);
+//
+//        return "firstfloor";
+//    }
+
+//    @PostMapping("/firstfloor/*")
+//    public String postdb(@ModelAttribute maas.maas.controllers.Inputs form, Model model) throws IOException, ExecutionException, InterruptedException{
+//        model.addAttribute("data", form);
+//        String id = form.getId();
+//
+//
+//
+//        System.out.println("clk "+id);
+//        model.addAttribute("logger",id);
+//        System.out.println(id);
+//        return "firstfloor";
+//    }
+
+
+
+
+
 }
+
+
+
