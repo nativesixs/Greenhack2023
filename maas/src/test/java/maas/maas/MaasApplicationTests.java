533e4cd3b60c7a6ee1ba56dfa536e033fe4ae5d5
nativesixs <matejzencak@seznam.cz>
2023-06-02 18:25:09 +0200

    floorplan and buttons unga bunga now

---
+++ maas/src/test/java/maas/maas/MaasApplicationTests.java
@@ -0,0 +1,13 @@
+package maas.maas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
